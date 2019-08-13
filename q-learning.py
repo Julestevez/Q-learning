875d6bdbb27478062d89be206bc3e4211671c533
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2019-08-13 21:36:40 +0200

    updated and cleaned

--- q-learning.py
+++ q-learning.py
@@ -8,6 +8,10 @@ import math
 import random
 import matplotlib.pyplot as plt
 
+
+#INITIALIZE VARIABLES
+######################
+
 m=1 #1kg mass
 g=9.80 #gravity
 dt=0.05 #simulation time
@@ -29,20 +33,13 @@ Rows=n_pos*n_speeds
 Columns=2
 Actions=([9.9, 9.7])
 
-n_items=302
 
-#initialize variables
+#time steps
+n_items=302
 x=np.linspace(0,301,n_items)
-z_pos_goal=np.zeros((1000, n_items))
-z_vel_goal=np.zeros((1000, n_items))
-z_acel_goal=np.zeros((1000, n_items))
-velocidad_final=np.zeros(n_items)
-z_sequence=np.zeros(n_items)
 
 #Initialize Q matrix
 Q=np.ones((Rows,Columns))
-goalState=101*n_speeds+11 #this is the index of the state where height=100cm and vel=0cm/s
-
 
 #Q-learning variables
 alpha=0.5
@@ -69,36 +66,34 @@ def ChooseAction (Columns,Q,state):
 
     return F, max_index
 
+#function to apply the dynamic model
 def ActionToState(F,g,m,dt,z_pos_old,z_vel_old,z_accel_old):
     z_accel=(-g + F/m)*100 
     z_vel=z_vel_old + (z_accel+z_accel_old)/2*dt
     z_pos=z_pos_old + (z_vel+z_vel_old)/2*dt
     z_accel_old=z_accel
-    z_vel_old=z_vel #temp
-    z_pos_old=z_pos #temp
+    z_vel_old=z_vel 
+    z_pos_old=z_pos 
 
     return z_accel,z_vel,z_pos,z_vel_old,z_pos_old
 
 
-
+#BEGINNING of the algorithm
 for episode in range(1,200000):
-    # random initial state
+    # initial state
     z_pos=np.zeros(n_items)
     z_vel=np.zeros(n_items)
     z_accel=np.zeros(n_items)
     z_pos_goal=np.zeros((1000, n_items))
     z_vel_goal=np.zeros((1000, n_items))
     z_acel_goal=np.zeros((1000, n_items))
-    # must do this to delete previous values
+    z_accel_old=0
+    z_vel_old=0
+    z_pos_old=0 #initial conditions of the particle
     
     state=11 #let's choose the initial state always height 0, speed 0cm/s
 
     print("episode",episode) #check
-
-    z_accel_old=0
-    z_vel_old=0
-    z_pos_old=0 #initial conditions of the particle
-
   
     for i in range(1,300):
 
@@ -120,13 +115,6 @@ for episode in range(1,200000):
             rounded_pos=round(z_pos[i])  #round the height  
             rounded_vel=round(z_vel[i])  #round the vel  
 
-                    
-             ## Find the maximum value of each row
-            QMax=max(Q[state]) 
-            max_indices=np.where(Q[state]==QMax)[0] # Identify all indexes where Q equals max
-            n_hits=len(max_indices) # Number of hits
-            max_index=int(max_indices[random.randint(0, n_hits-1)]) # If many hits, choose randomly
-            
             #calculate which is my new state
             index_1=np.where(STATES==rounded_pos)
             index_2=np.where(SPEEDS==rounded_vel)
@@ -144,6 +132,23 @@ for episode in range(1,200000):
 
             #Q VALUE update
             Q[state,max_index]=Q[state,max_index] + alpha*(Reward + gamma*(QMax - Q[state,max_index]))  #update Q value
+                       
+
+            #checking
+            if (rounded_pos==100 or rounded_pos==99 or rounded_pos==101):
+                print("entra")
+                if (rounded_vel==0):
+                    goalCounter=goalCounter+1 #counter of successful hits
+                    
+                    #saving of successful data
+                    z_pos_goal[0:i,goalCounter]=z_pos[0:i]
+                    z_vel_goal[0:i,goalCounter]=z_vel[0:i]
+                    z_acel_goal[0:i,goalCounter]=z_accel[0:i]
+                    state=11 #reinitialize
+                    break
+
+                else:
+                    break
                        
 
             #checking
